2b52ee56e50e3836f95b454a7d2fd13104914f72
ejembi <ejembiedwin36@gmail.com>
2026-09-21 13:54:30 +0100

    my 30 days of python

---
+++ Day1_Introduction/execise2.0.py
@@ -0,0 +1,52 @@
+# Write an example for different Python data types such as 
+# Number(Integer, Float, Complex), String, Boolean, 
+# List, Tuple, Set and Dictionary.
+
+1. Number 
+Integer are number with no decimal or fractional part 
+Examples:
+Positive whole number: 1, 2, 3, 4, 5....
+zero: 0
+Negative: -1, -2, -3, -4, -5 .....
+
+Float: are numbers whit decimal point
+Examples: 0.0, 1.1, 2.25, 2.2...
+
+String is collection of one or mire characters under a double or single quote.
+Examples: 
+"I you guys are doing well whit you studies"
+'Hello world'.
+
+Boolean is either a True or False value.
+Examples:
+is 8 greater than 9:
+True or 
+False .
+
+
+List is a collection multiple data store in a single ariable.
+Examples:
+[apple, bananer, mango, orange].
+[1, 2, 3, 4, 5, 6]
+
+
+Dictionary is a collection of key.value pairs
+{
+    'first_name':'monday',
+    'last_name':'ocheme',
+    'age':24,
+    'contry_name':'Nigeria'
+}
+
+
+Tuple is an ordered group of element or value.
+Example:
+
+('Ada', 'Mercy', 'rose')
+
+
+
+Set is a collection of data type similar to tuple.
+
+Example: {2, 4, 3, 5}
+{3.15, 8.32, 3.5}
