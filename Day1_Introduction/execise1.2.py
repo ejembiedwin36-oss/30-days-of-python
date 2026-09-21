de4bc923f5c059bbd7fc55a57019041e787cb283
ejembi <ejembiedwin36@gmail.com>
2026-09-21 11:45:34 +0100

    my 30-Days-of-python

---
+++ Day1_Introduction/execise1.2.py
@@ -0,0 +1,24 @@
+#Write strings on the python interactive shell. 
+#The strings are the following:
+
+print("Edwin Ejembi")
+print("Ejembi's Family")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+
+#the result on my terminal
+
+"""
+ Day1_Introduction git:(main) ✗ python3
+Python 3.12.3 (main, Feb  4 2025, 14:48:35) [GCC 13.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> "Edwin Ejembi"
+'Edwin Ejembi'
+>>> "Ejembi's Family"
+"Ejembi's Family"
+>>> "Nigeria"
+'Nigeria'
+>>> "i am enjoying 30 days of python"
+'i am enjoying 30 days of python'
+>>> 
+"""
