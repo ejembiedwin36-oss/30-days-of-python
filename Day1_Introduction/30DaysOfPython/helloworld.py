150d74a7ffbab15454cb7a2bf576941f6af3a1c5
ejembi <ejembiedwin36@gmail.com>
2026-09-21 12:15:11 +0100

    my 30 days of python

---
+++ Day1_Introduction/30DaysOfPython/helloworld.py
@@ -0,0 +1,71 @@
+# question 1
+# Check the python version you are using
+python3 --version
+python 3.12.3
+
+#I am currently using python 3.12.3
+
+
+# question 2
+# Write strings on the python interactive shell. 
+# The strings are the following:
+
+print(3 + 4)     # addition(+)
+print(4 - 3)     # subtraction(-)
+print(3 * 4)     # multiplication(*)
+print(4 % 3)     # modulus(%)
+print(4 / 3)     # division(/)
+print(4 ** 3)    # exponential(**)
+print(4 // 3)    # floor division operator(//)
+
+# question 3
+#Write strings on the python interactive shell. 
+#The strings are the following:
+
+print("Edwin Ejembi")
+print("Ejembi's Family")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+
+#the result on my terminal
+
+"""
+ Day1_Introduction git:(main) ✗ python3
+Python 3.12.3 (main, Feb  4 2025, 14:48:35) [GCC 13.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> "Edwin Ejembi"
+'Edwin Ejembi'
+>>> "Ejembi's Family"
+"Ejembi's Family"
+>>> "Nigeria"
+'Nigeria'
+>>> "i am enjoying 30 days of python"
+'i am enjoying 30 days of python'
+>>> 
+"""
+
+
+# question 4
+# Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type("Edwin Ejembi"))
+print(type("Ejembi's Family"))
+print(type("Nigeria"))
+
+# terminal result 
+"""
+Day1_Introduction git:(main) ✗ python3 execise1.3.py
+<class 'int'>
+<class 'float'>
+<class 'float'>
+<class 'complex'>
+<class 'list'>
+<class 'str'>
+<class 'str'>
+<class 'str'>
+➜  Day1_Introduction git:(main) ✗ 
+"""
