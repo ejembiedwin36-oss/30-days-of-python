43a22aff74733206f5968b7b10ba5895faae9009
ejembi <ejembiedwin36@gmail.com>
2026-09-21 11:54:58 +0100

    my 30 days of python

---
+++ Day1_Introduction/execise1.3.py
@@ -0,0 +1,23 @@
+# Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type("Edwin Ejembi"))
+print(type("Ejembi's Family"))
+print(type("Nigeria"))
+
+# terminal result 
+"""
+Day1_Introduction git:(main) ✗ python3 execise1.3.py
+<class 'int'>
+<class 'float'>
+<class 'float'>
+<class 'complex'>
+<class 'list'>
+<class 'str'>
+<class 'str'>
+<class 'str'>
+➜  Day1_Introduction git:(main) ✗ 
+"""
