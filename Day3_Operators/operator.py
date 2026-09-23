8bf48be53b5f9782548a1c0981077a2f7261a3b8
Edwin Ejembi <ejembiedwin36@gmail.com>
2026-09-23 15:32:36 +0100

    some of the 30 days of python

---
+++ Day3_Operators/operator.py
@@ -0,0 +1,36 @@
+age = 26
+height = 9.75
+store = 3 + 2j
+print(age, height, store)
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+area = 0.5 * base * height
+#print(area)# it work too
+print(f'Area of triangle is {area}')"""
+
+"""side_a = int(input("Enter the perimeter of the triangle: "))
+side_b = int(input("Enter the perimeter of the triangle: "))
+side_c = int(input("Enter the perimeter of the triangle: "))
+
+perimeter = side_a + side_b + side_c 
+print(f'The perimeter of tringle is {perimeter}')
+
+
+lenght = 10 
+width = 20
+area =  lenght * width
+perimeter = 2 * (lenght + width)
+print(area, perimeter)
+
+pi = 3.14
+radius = float(input("Enter the radius of the circle: "))
+
+area = pi * radius * radius
+circumfrence = 2 * pi * radius
+
+print(f"Area: {area}")
+print(f"Circumfrence: {circumfrence}")
+
+
+
