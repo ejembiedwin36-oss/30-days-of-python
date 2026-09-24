b20da5ca7040fd3cab5fb00b258ed03fc9550305
ejembi <ejembiedwin36@gmail.com>
2026-09-24 23:37:11 +0100

    new work of day four

---
+++ Day_4_strings/string.py
@@ -0,0 +1,11 @@
+# concatenating a string.
+result = 'Thirty' + ' ' + 'Days' + ' ' + 'Of' + ' ' + 'Python'
+print(result)
+word = 'Coding' + ' ' + 'For' + ' ' + 'All'
+print(word)
+company = "Codine For All"
+print(company)
+output = len(company)
+print(output)
+answer = ('coding for all'.upper())
+print(answer)
